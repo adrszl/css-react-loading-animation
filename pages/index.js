7020a73e3b7c1fb451d5a5e3365eac9dbef04dee
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-06-25 14:02:05 +0200

    added new header & added footer

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   const [loadingState, updateLoadingState] = useState(true);
   const [contentState, updateContentState] = useState({
     header: 'Loading',
+    subHeader: 'Loading',
     intro: 'Loading',
     list: [
       'Loading',
@@ -13,13 +14,14 @@ export default function Home() {
   })
 
   const content = {
-    header: `So, how 'bout them Knicks?`,
+    header: `React loading animation for placeholder`,
+    subHeader: `So, how 'bout them Knicks?`,
     intro: `What are their names? I'm Santa Claus! This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Good news, everyone! I've taught the toaster to feel love!`,
     list: [
       `Yes! In your face, Gandhi!`,
       `So I really am important? How I feel when I'm drunk is correct?`,
       `Who are those horrible orange men?`,
-    ],
+    ]
   };
 
   useEffect(() => {
@@ -33,6 +35,7 @@ export default function Home() {
     <div className="container">
       <main className={loadingState ? 'loading' : ''}>
         <h1>{contentState.header}</h1>
+        <h2>{contentState.subHeader}</h2>
         <p>{contentState.intro}</p>
         <ul>
           {Array.isArray(contentState.list) &&
@@ -41,6 +44,9 @@ export default function Home() {
             })}
         </ul>
       </main>
+      <footer className={loadingState ? 'loading' : ''}>
+        {loadingState ? "Loading..." : <span>Want to see other projects? Check them <a href="https://adrian-szlegel-personal-projects.netlify.app/" target="_blank" style={{color: "blue", texDecoration: "underline"}}>here</a></span>}
+      </footer>
     </div>
   );
 }
