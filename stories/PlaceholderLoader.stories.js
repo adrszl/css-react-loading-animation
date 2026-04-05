a05b2cf6c52912f307adcb3ca30393ac2460f83c
Adrian Szlegel <adrian95999@gmail.com>
2026-04-05 22:11:07 +0200

    first story added

---
+++ stories/PlaceholderLoader.stories.js
@@ -0,0 +1,16 @@
+import { PlaceholderLoader } from "./PlaceholderLoader";
+import './placeholderLoader.css';
+
+export default {
+    title: 'Simple paragraph skeleton/PlaceholderLoader',
+    component: PlaceholderLoader,
+    parameters: {
+        layout: 'fullscreen',
+    },
+    tags: ['autodocs'],
+};
+
+export const Primary = {
+    args: {
+    },
+};
